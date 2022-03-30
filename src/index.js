0792421612f322f6205682e53b1bf66e6d52e423
Vincent Tran <vincentuong@yahoo.com>
2022-03-29 20:40:00 -0500

    commit tree 72d02aba42ad26198819676525832ffb2de578db

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-// ./src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import counterReducer from "./reducers/counterReducer";
+import App from "./App";
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import counterReducer from './reducers/counterReducer.js';
-import App from './App';
-import './index.css';
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); /* code change */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App store={store} />,
+  document.getElementById("root")
+);
